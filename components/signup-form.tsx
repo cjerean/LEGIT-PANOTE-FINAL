ee1e725b3a506d20962a62525491b8b20e1d1537
Romaaanoo-7 <161458882+Romaaanoo-7@users.noreply.github.com>
2025-12-09 17:40:24 +0800

    login and signup page updated

---
+++ components/signup-form.tsx
@@ -0,0 +1,76 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+export function SignupForm({ className, ...props }: React.ComponentProps<typeof Card>) {
+  return (
+    <Card className={cn("border-2 border-primary", className)} {...props}>
+      <CardHeader>
+        <CardTitle>Create an account</CardTitle>
+        <CardDescription>
+          Enter your information below to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="name">Full Name</FieldLabel>
+              <Input className="border-2 border-primary" id="name" type="text" placeholder="EJ SUPOT" required />
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                className="border-2 border-primary"
+                id="email"
+                type="email"
+                placeholder="ej.supot
+                @example.com"
+                required
+              />
+              <FieldDescription>
+                We&apos;ll use this to contact you. We will not share your email
+                with anyone else.
+              </FieldDescription>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <Input className="border-2 border-primary" id="password" type="password" required />
+              <FieldDescription>
+                Must be at least 8 characters long.
+              </FieldDescription>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="confirm-password">
+                Confirm Password
+              </FieldLabel>
+              <Input className="border-2 border-primary" id="confirm-password" type="password" required />
+              <FieldDescription>Please confirm your password.</FieldDescription>
+            </Field>
+            <FieldGroup>
+              <Field>
+                <Button type="submit">Create Account</Button>
+                <FieldDescription className="px-6 text-center">
+                  Already have an account? <a href="/login">Sign in</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
